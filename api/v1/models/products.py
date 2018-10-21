cd0eb54f9ad5d082e7722d7d9664511b53fc7a15
e-ian <e-ian>
2018-10-21 03:06:10 +0300

    Refactor code

--- api/v1/models/products.py
+++ api/v1/models/products.py
@@ -4,32 +4,38 @@ module products class in models
 
 from flask import Flask, jsonify, make_response, request
 class Products():
+    product_list = []
 
-    def __init__(self):
-        self.product_list = []
+    def __init__(self, product_name, category, price, quantity, minimum_quantity):
+        self.product_name = product_name
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+        self.minimum_quantity = minimum_quantity
 
-    def add_product(self, product_name, category, price, quantity, minimum_quantity):
-        product_dict = {}
-        product_dict['product_id'] = len(self.product_list) + 1
-        product_dict['product_name'] = product_name
-        product_dict['category'] = category
-        product_dict['price'] = price
-        product_dict['quantity'] = quantity
-        product_dict['minimum_quantity'] = minimum_quantity
+    def json_format(self):
+        """
+        returns data in json format
+        """
+        return self.__dict__
+    @classmethod
+    def len_of_dict(cls):
+         return len(cls.product_list) + 1
 
-        #appending to list
-        self.product_list.append(product_dict)
-        message = {
-            'message': 'Product successfully added'
-        }
-
-        return make_response(jsonify(message), 201)
-
-    def get_products(self):        
-        return self.product_list
-
-    def get_single_product(self, product_id):
-        for product in self.product_list:
+    @classmethod
+    def add_product(cls, product):
+        """
+        class method to add a product to product_list
+        """
+        cls.product_list.append(product)
+        return cls.product_list
+    @classmethod
+    def get_products(cls):        
+        return cls.product_list
+    
+    @classmethod
+    def get_single_product(cls, product_id):
+        for product in cls.product_list:
             if product['product_id'] == product_id:
                 return product
 
