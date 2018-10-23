6a6738a96ac031cf074a202ee1c68480fea3c0a4
e-ian <e-ian>
2018-10-24 02:26:24 +0300

    Refactor code to add validations

--- api/v1/models/sales.py
+++ api/v1/models/sales.py
@@ -2,32 +2,54 @@
 defines sales class in models
 """
 from flask import Flask, jsonify, make_response, request
-class Sales():
+class Sales:
+    sale_orders = []
 
-    def __init__(self):
-        """initializes sales_orders list"""
-        self.sales_orders = []
-    
-    def add_sale_order(self, product_name, price, quantity):
-        sales_dict = {}
-        sales_dict['sale_id'] = len(self.sales_orders) + 1
-        sales_dict['product_name'] = product_name
-        sales_dict['price'] = price
-        sales_dict['quantity'] = quantity       
+    def __init__(self, product_name, price, quantity):
+        """initializes Sales"""
+        self.product_name = product_name
+        self.price = price
+        self.quantity = quantity
 
-        #appending to list of sale_orders
+    # def to_json_format(self):
+    #     """
+    #     returns data in json format
+    #     """
+    #     return self.__dict__
+    @classmethod
+    def len_of_orders(cls):
+        return len(cls.sale_orders) + 1
 
-        self.sales_orders.append(sales_dict)
-        message = {
-            'message': 'sale order added successfully'
-        }
+    @classmethod
+    def add_sale_order(cls, sale):
+        """
+        class method to add a sale order to sale_order
+        """
+        cls.sale_orders.append(sale)
+        return cls.sale_orders
 
-        return make_response(jsonify(message), 201)
+    @classmethod
+    def fetch_sale_orders(cls):
+        return cls.sale_orders
 
-    def fetch_sale_orders(self):        
-        return self.sales_orders
-
-    def fetch_specific_sale_record(self, sale_id):
-        for sale_order in self.sales_orders:
+    @classmethod
+    def fetch_specific_sale_record(cls, sale_id):
+        for sale_order in cls.sale_orders:
             if sale_order['sale_id'] == sale_id:
                 return sale_order
+
+    def validate_sale_order(self):
+        if self.product_name.strip() == '':
+            return jsonify({'error': "product name cannot be an empty string"}), 400 
+        if not self.product_name.isalpha():
+            return jsonify({'error': "product name should have alphabet letters only"}), 400         
+        if isinstance(self.price, str):
+            return jsonify({'error': "price cannot be a string"}), 400
+        if not isinstance(self.price, int):
+            return jsonify({'error': "input price as integer"}), 400
+        if isinstance(self.quantity, str):
+            return jsonify({'error': "quantity cannot be a string"}), 400
+        if not isinstance(self.quantity, int):
+            return jsonify({'error': "input quantity as integer"}), 400
+        return True
+    
