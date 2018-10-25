8748aa9a63e87e08d2f381cbd5bd72afa7a8e9ce
e-ian <e-ian>
2018-10-25 15:09:18 +0300

    Refactor code to PEP8 style

--- api/v1/models/products.py
+++ api/v1/models/products.py
@@ -2,45 +2,48 @@
 module products class in models
 """
 
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify
 
 class Products:
+    """class for products model"""
     product_list = []
 
-    def __init__(self, product_name, category, price, quantity, minimum_quantity):
-        self.product_name = product_name
-        self.category = category
-        self.price = price
-        self.quantity = quantity
-        self.minimum_quantity = minimum_quantity
-   
+    def __init__(self, *args):
+        self.product_name = args[0]
+        self.category = args[1]
+        self.price = args[2]
+        self.quantity = args[3]
+        self.minimum_quantity = args[4]
+
     @classmethod
     def len_of_dict(cls):
+        """class method that returns length of product_list with increment"""
         return len(cls.product_list) + 1
 
     @classmethod
     def add_product(cls, product):
-        """
-        class method to add a product to product_list
-        """
+        """class method to add a product to product_list """
         cls.product_list.append(product)
         return cls.product_list
+
     @classmethod
-    def get_products(cls):        
+    def get_products(cls):
+        """class method to return all products"""
         return cls.product_list
-    
+
     @classmethod
     def get_single_product(cls, product_id):
+        """class method to return a single product"""
         for product in cls.product_list:
             if product['product_id'] == product_id:
                 return product
-      
-    def validate_product_name_input(self):
-        
+
+    def validate_product_post_input(self):
+        """method for validating inputs"""
         if self.product_name.strip() == '':
             return jsonify({'error': "product name cannot be an empty string"}), 400
         if not self.product_name.isalpha():
-            return jsonify({'error': "product name should have alphabet letters only"}), 400                   
+            return jsonify({'error': "product name should have alphabet letters only"}), 400   
         if isinstance(self.price, str):
             return jsonify({'error': "price cannot be a string"}), 400
         if not isinstance(self.price, int):
@@ -48,12 +51,12 @@ class Products:
         if isinstance(self.quantity, str):
             return jsonify({'error': "quantity cannot be a string"}), 400
         if not isinstance(self.quantity, int):
-            return jsonify({'error': "input quantity as integer"}), 400 
+            return jsonify({'error': "input quantity as integer"}), 400
         if isinstance(self.minimum_quantity, str):
             return jsonify({'error': "minimum quantity cannot be a string"}), 400
         if not isinstance(self.minimum_quantity, int):
-            return jsonify({'error': "input minimum quantity as integer"}), 400        
+            return jsonify({'error': "input minimum quantity as integer"}), 400
         if self.category.strip() == '':
-            return jsonify({'error': "category cannot be an empty string"}), 400                   
-        return True     
+            return jsonify({'error': "category cannot be an empty string"}), 400
+        return True
     
