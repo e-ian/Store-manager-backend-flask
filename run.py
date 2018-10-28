b7b36530150982a90eb28d8bad1b22f5367d34ac
e-ian <e-ian>
2018-10-29 00:19:28 +0300

    modifies run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-"""
-module run
-"""
+"""module to run flask app"""
+from api.v1.models import Datastore
+from flask import Flask
 
-from api.v1 import app
+app = Flask(__name__)
 
 if __name__ == '__main__':
+    db_connect = Datastore()
+    db_connect.create_user_table()
+    db_connect.create_products_table()
+    db_connect.create_sales_table()
     app.run(debug=True)
