64855a09a5c657270858d79957cca08d35e2895b
e-ian <e-ian>
2018-10-30 17:17:48 +0300

    Modifies the init file in api

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from config import DevelopmentConfig
 
-def create_app(DevelopmentConfig):
-    app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
-    return app
+app = Flask(__name__)
+
+from api.v1.views import views
+
+
+# from flask import Flask
+# from config import DevelopmentConfig
+
+# def create_app(DevelopmentConfig):
+#     app = Flask(__name__)
+#     app.config.from_object(DevelopmentConfig)
+#     return app
 
