b381a89cfa77fbe50da40cd8f46979d64d28db81
e-ian <e-ian>
2018-10-30 17:18:28 +0300

    modifies db_actions file

--- api/v1/db_actions.py
+++ api/v1/db_actions.py
@@ -19,7 +19,59 @@ class Products:
         cursor.execute(query)
         return data
 
-        # query = "INSERT INTO products(product_name, category, price, \
-        # quantity, minimum_quantity) VALUES('shoe', 'mens clothing', '3000', '1', '5')"
-        # cursor.execute(query)
-        # return True
+    def get_products(self):
+        """method that returns all products"""
+        query = "SELECT * FROM products"
+        dictcur.execute(query)
+        products = dictcur.fetchall()
+        return products
+
+    def get_single_product(self, product_id):
+        """method that returns a single product"""
+        query = "SELECT * FROM products WHERE product_id='{}'".format(product_id)
+        dictcur.execute(query)
+        single_product = dictcur.fetchone()
+        return single_product
+
+    def modify_product(self, product_id, product_name, price, category, quantity, minimum_quantity):
+        """modifies or edits a single product"""
+        query = "UPDATE products SET product_name='{}', price='{}', category='{}', \
+        quantity='{}', minimum_quantity='{}' WHERE product_id='{}'".format(product_name, \
+        price, category, quantity, minimum_quantity, product_id)
+        dictcur.execute(query)
+        return dictcur
+        
+class Sales:
+    """Class handling all operations on sales"""
+    def add_sale_order(self, data):
+        """method to add sale order"""
+        query = "INSERT INTO sales(product_name, price, quantity) \
+        VALUES('{}', '{}', '{}')".format(data['product_name'], data['price'], data['quantity'])
+        cursor.execute(query)
+        return data
+
+    def get_sale_orders(self):
+        """method to get all sale orders"""
+        query = "SELECT * FROM sales"
+        dictcur.execute(query)
+        sales = dictcur.fetchall()
+        return sales
+    def get_specific_sale_order(self, sale_id):
+        """method to get a specific sale order"""
+        query = "SELECT * FROM sales WHERE sale_id='{}'".format(sale_id)
+        dictcur.execute(query)
+        specific_sale = dictcur.fetchone()
+        return specific_sale
+
+class Users:
+    """Class handling all db actions on users"""
+
+    def register_user(self, data):
+        """method to register a new user"""
+        query = "INSERT INTO users(username, password, role) \
+        VALUES('{}', '{}', 'user')".format(data['username'], \
+        generate_password_hash(data['password']))
+        cursor.execute(query)
+        return data
+
+
