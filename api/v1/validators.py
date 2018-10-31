75a2d1591f8193cff11a3479afd400095a28ecd5
e-ian <e-ian>
2018-10-31 12:30:17 +0300

    Adds validations for register user endpoint

--- api/v1/validators.py
+++ api/v1/validators.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import re
 class Validate:
     def __init__(self, *args):
         self.product_name = args[0]
@@ -17,7 +18,7 @@ class Validate:
 
     def validate_price_and_quantity(self):
         """method to validate price, quantity and minimum quantity inputs"""
-        if not self.price or not self.quantity or not self.minimum_quantity or not isinstance(self.price, int) \
+        if self.price or not self.quantity or not self.minimum_quantity or not isinstance(self.price, int) \
          or not isinstance(self.quantity, int) or not isinstance(self.minimum_quantity, int):
             return False
         else:
@@ -38,3 +39,27 @@ class Validate:
             return False
         else:
             return True
+
+    @staticmethod
+    def validate_input_str(input_str):
+        """method to validate if input is string"""
+        if re.search(r'\s', input_str):
+            return jsonify({'error': "input field cannot have empty spaces"}), 400
+        if re.search(r'\d', input_str):
+            return jsonify({'error': "input field shouldnot have digits but letters"}), 400
+        if re.search(r'\W', input_str):
+            return jsonify({'error': "input field should have alphabets only"}), 400
+
+    @staticmethod
+    def validate_password(password):
+        """method to validate password input"""
+        if not len(password) >= 6:
+            return jsonify({"error": "password length should be equal or greater than 6"}), 400
+        if not re.search(r'\d', password):
+            return jsonify({"error": "password should contain a digit"}), 400
+        if not re.search(r'\W', password):
+            return jsonify({"error": "password should contain some alpha numeric characters"}), 400
+        
+
+
+   
