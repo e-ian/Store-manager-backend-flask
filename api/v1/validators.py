bec97b69fbbd24ac18fbe1ed5ce2c100c5bd37d8
e-ian <e-ian>
2018-10-31 18:07:51 +0300

    Adds validations to register a user

--- api/v1/validators.py
+++ api/v1/validators.py
@@ -1,25 +1,20 @@
 from flask import Flask, jsonify
 import re
-class Validate:
-    def __init__(self, *args):
-        self.product_name = args[0]
-        self.category = args[1]
-        self.price = args[2]
-        self.quantity = args[3]
-        self.minimum_quantity = args[4]
+class Validate:  
 
-    def validate_prod_name_and_category(self):
+    @staticmethod
+    def validate_prod_name_and_category(product_name, category):
         """method to validate product_name and category inputs"""
-        if not self.product_name or not self.category or self.product_name.isspace() or self.category.isspace() \
-        or not isinstance(self.product_name, str) or not isinstance(self.category, str):
+        if not product_name or not category or product_name.isspace() or category.isspace() \
+        or not isinstance(product_name, str) or not isinstance(category, str):
             return False
         else:
             return True
-
-    def validate_price_and_quantity(self):
+    @staticmethod
+    def validate_price_and_quantity(price, quantity, minimum_quantity):
         """method to validate price, quantity and minimum quantity inputs"""
-        if self.price or not self.quantity or not self.minimum_quantity or not isinstance(self.price, int) \
-         or not isinstance(self.quantity, int) or not isinstance(self.minimum_quantity, int):
+        if not price or not quantity or not minimum_quantity or not isinstance(price, int) \
+        or not isinstance(quantity, int) or not isinstance(minimum_quantity, int):
             return False
         else:
             return True
