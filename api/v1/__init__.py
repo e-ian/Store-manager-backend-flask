aea1f1c99c7615b838c3b63aa9b86ee08600dd60
e-ian <e-ian>
2018-11-01 10:31:30 +0300

    Modifies the __init__.py file

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
+from api.v1.models import Datastore 
 
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'akokoro'
 jwt = JWTManager(app)
+db_connect = Datastore()
+db_connect.create_user_table()
+db_connect.create_products_table()
+db_connect.create_sales_table()
 from api.v1.views import views
+
 
 
 
